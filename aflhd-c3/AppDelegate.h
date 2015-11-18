82f540e89a1fe5e85375a13e8656deeefbb0980d
tkumata <tkumata@gmail.com>
2015-11-18 12:19:17 +0900

    Initial Commit

---
+++ aflhd-c3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aflhd-c3
+//
+//  Created by KUMATA Tomokatsu on 11/18/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aflhd-c3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aflhd-c3
+//
+//  Created by KUMATA Tomokatsu on 11/18/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
